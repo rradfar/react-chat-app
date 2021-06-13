2d29d922a8521792dcfd06e0d4e1239b47caa56b
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-06-13 06:43:19 -0700

    Add AuthProvider

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,13 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { auth } from '../firebase';
+
+const AuthContext = React.createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState({});
+};
