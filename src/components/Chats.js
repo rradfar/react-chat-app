d3f29727c9b9a916314f7eadcc50ddc29b2b57f0
Ramtin <31255934+rradfar@users.noreply.github.com>
2021-06-19 07:14:42 -0700

    Set up the user - wip

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useEffect, UseRef, UseState } from 'react';
 import { useHistory } from 'react-router';
 import { ChatEngine } from 'react-chat-engine';
 
 import { auth } from '../firebase';
 import { useAuth } from '../contexts/AuthContext';
+import axios from 'axios';
 
 export default function Chats() {
   const history = useHistory();
   const { user } = useAuth();
+  const { loading, setLoading } = useState(true);
 
   const handleLogout = async () => {
     await auth.signOut();
     history.push('/');
   };
+
+  const getFile = async url => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File({ data }, 'userPhoto.jpg', { type: 'image/jpeg' });
+  };
+
+  useEffect(() => {
+    if (!user) {
+      history.push('/');
+      return;
+    }
+    axios
+      .get('https://api.chatengine.io/users/me', {
+        headers: {
+          'project-ID': '784bdb9e-8724-4f63-8ab6-3c10d59f74a7',
+          'user-name': user.email,
+          'user-secret': user.uid,
+        },
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        let formData = new FormData();
+        formData.append('email', user.email);
+        formData.append('username', user.displayName);
+        formData.append('secret', user.uid);
+        getFile(user.photoUrl).then(avatar => {
+          formData.append('avatar', avatar, avatar.name);
+        });
+        axios
+          .post('https://api.chatengine.io/users', formData, {
+            headers: {
+              'private-key': 'placeholder-private-key',
+            },
+          })
+          .then(() => setLoading(false))
+          .catch(error => console.log(error));
+      });
+  }, [user, history]);
 
   return (
     <div className='chats-page'>
